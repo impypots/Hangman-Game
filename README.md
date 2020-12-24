9601e624e19f00ecfb6c1a1bbe853593dbbf3f61
impypots <72578093+impypots@users.noreply.github.com>
2020-12-23 18:31:57 -0700

    It works now!
    It works now! Still want to make some small tweaks before I move onto another project.

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 
  12/22/2020
  Made some progress with regards to how I'm going to display the correctly guessed letters of the secret word in an ordered fashion. Still kind of unsure on how I'm going to make that work. In the meantime, I made the hangman images, and set them up to change with each incorrect guess.
+
+ 12/23/2020
+ It works now! Learned how to display the correctly guessed letters of the secret word. Tried to make it look a bit more appealing. The function now removes the event listener once the player wins/loses so they can't continue to input letters after the game ends. I think tomorrow I'm going to change the game from "Hangman" to "Reverse Hangman". Just for fun.
--- index.js
+++ index.js
@@ -4,26 +4,38 @@ let secret = document.getElementById("secret");
 let incorrect = document.getElementById("incorrect-letters");
 let hangman = document.getElementById("hangman");
 //list of words from which the word the player will try to guess is pulled.
-let words = ["nancy", "greg", "kitten", "nuclear", "kitchen", "sunny", "moonlight", "cookie", "fish", "sheep", "kangaroo", "related", "kick", "bubbles", "bouncy", "offend", "interest", "hound"];
+let words = ["nancy", "greg", "kitten", "nuclear", "kitchen", "sunny", "moonlight", "cookie", "shiny", "fish", "sheep", "silly", "kangaroo", "related", "kick", "bubbles", "bouncy", "offend", "interest", "hound", "lady", "xylophone", "oscar", "airplane", "finger", "morose", "morbid"];
 let incorrectCounter = 0; //this variable will keep track of the number of incorrect guesses.
 //word to be guessed by player
 let secretWord = words[Math.floor(Math.random() * words.length)];
 let secretArr = secretWord.split("");
 console.log(secretArr);
 
-secret.innerText = `Secret Word(${secretWord.length}): `;
-button.addEventListener("click", ()=> {
+//replaces elements in secretArr with underscores so player can see how many letters are in each word.
+for(let i = 0; i < secretArr.length; i++){
+    secretArr[i] = "_"
+}
+
+function buttonClick() {
     if(input.value.length > 1){
         alert("Just one letter at a time, please!")
     } else if (secretWord.includes(input.value)){
-        alert("That's in secretWord!") //placeholder to test functionality.
-        secret.innerText += secretArr.filter(x => x === input.value).join("");
-        console.log(secretArr.filter(x => x === input.value))
+        //checks to see if the letter at position i in secretWord is equal to the user input. If it is, the underscore at position i in secretArr is replaced with the user input.
+        for(let i = 0; i < secretArr.length; i++){
+            if(secretWord[i] === input.value){
+                secretArr[i] = input.value;
+            }
+        }
+        secret.innerText = `Secret Word(${secretArr.length}): ${secretArr.join(" ")}`;
+        if(secretArr.join("") === secretWord){
+            alert("You win! Now we just need to reform our cruel and unusual death sentencing procedures!");
+            button.removeEventListener("click", buttonClick);
+        }
     } else {
-        alert("That's not in secretWord!"); //placeholder to test functionality.
         incorrect.innerText += input.value;
         input.value = "";
         incorrectCounter += 1;
+        //this switch case changes the hangman image based upon the number of incorrect guesses the player has made.
         switch (incorrectCounter) {
             case 1:
                 hangman.src = "C:/Users/Owner/Documents/GitHub/Hangman-Game/hangman2.png";
@@ -42,8 +54,14 @@ button.addEventListener("click", ()=> {
                 break;
             case 6:
                 hangman.src = "C:/Users/Owner/Documents/GitHub/Hangman-Game/hangman7.png";
+                alert("You lost the word guessing game! And therefore, this man will now be executed... for some reason.");
+                secret.innerText = `Secret Word(${secretArr.length}): ${secretWord.split("").join(" ")}`;
+                button.removeEventListener("click", buttonClick);
                 break;
         }
     }
     input.value = ""
-}, false)
+}
+
+secret.innerText = `Secret Word(${secretArr.length}): ${secretArr.join(" ")}`;
+button.addEventListener("click", buttonClick, false)
